9811ac489be51efbdb3e67e721b2dba9bd5fdf22
Naitik Mundra <mundranaitik@outlook.com>
2023-04-07 22:19:06 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 /*
- * Copyright (c) 2023.
+ * Copyright (c)  2023 Naitik Mundra.
  *
- * Copyright (C) <year>  <name of author>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
